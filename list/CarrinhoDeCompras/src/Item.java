93a81c2e6fdb3c09ab1b60e9be817a4e56b8b539
FelipeRodrii <feliperodr2701@gmail.com>
2023-10-15 01:24:48 -0300

    SubindoExerciciosList

---
+++ list/CarrinhoDeCompras/src/Item.java
@@ -0,0 +1,28 @@
+
+public class Item {
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    public Item(String nome, double preco, int quantidade ){
+        this.nome= nome;
+        this.preco= preco;
+        this.quantidade= quantidade;
+    }
+
+    public String getNomeDoItem(){
+        return nome;
+    }
+
+    public double getValorDoItens(){
+        return preco;
+    }
+
+    public int getQuantidadeDoItens(){
+        return quantidade;
+    }
+
+    public String mostrarItem(){
+        return"Preço: "+ nome+" Valor: "+preco+" Un: "+quantidade;
+    }
+}
